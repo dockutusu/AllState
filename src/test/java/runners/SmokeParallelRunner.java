f39f29aa7497c12180308bca05b401180ae08077
dockutusu <dockutusu@gmail.com>
2020-08-16 13:35:35 -0400

    added parallel execution

--- src/test/java/runners/SmokeParallelRunner.java
+++ src/test/java/runners/SmokeParallelRunner.java
@@ -18,8 +18,17 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 		monochrome = true
 
 		)
-public class SmokeRunner {
+public class SmokeParallelRunner extends AbstractTestNGCucumberTests {
 
-		
+	@Override
+	@DataProvider (parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+	
+	
+	
+ 
+	
 
 }
