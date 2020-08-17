e7d24f297d8b69168dcacdcf3959857f6e6655d8
dockutusu <dockutusu@gmail.com>
2020-08-16 20:52:34 -0400

    headless

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import utils.ConfReader;
 import utils.Constants;
 
@@ -17,7 +19,15 @@ public class BaseClass {
 		switch (ConfReader.getConfigValue("browser").toLowerCase()) {
 		case "chrome":
 			System.setProperty("webdriver.chrome.driver", Constants.CHROME_PATH);
-			driver = new ChromeDriver();
+
+			if (ConfReader.getConfigValue("headless").equals("true")) {
+				ChromeOptions options = new ChromeOptions();
+				options.setHeadless(true);
+				driver = new ChromeDriver(options);
+			} else {
+				driver = new ChromeDriver();
+			}
+
 		}
 		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
 	}
